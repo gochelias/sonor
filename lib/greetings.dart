0f8a835a7f98fde5207e0f0f2fa78781ad45da02
gochelias <56095957+gochelias@users.noreply.github.com>
2022-12-12 13:07:06 -0600

    fix: greetings have been corrected

--- lib/greetings.dart
+++ lib/greetings.dart
@@ -1,10 +1,14 @@
 String greetings() {
   final hour = DateTime.now().hour;
+  String greet = 'Hello 👋';
 
   if (hour < 12) {
-    return 'Good morning 👋';
-  } else if (hour > 12 && hour < 18) {
-    return 'Good afternoon 👋';
+    greet = 'Good morning 👋';
+  } else if (hour >= 12 && hour <= 17) {
+    greet = 'Good afternoon 👋';
+  } else if (hour >= 18) {
+    greet = 'Good night 👋';
   }
-  return 'Good night 👋';
+
+  return greet;
 }
