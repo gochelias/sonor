479dbb1878f776ac354670cc3631787cd83bb5ef
gochelias <56095957+gochelias@users.noreply.github.com>
2022-12-26 12:58:54 -0600

    chore: the home screen has been added to the screens file

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,4 +1,4 @@
 export 'package:sonor/screens/explore_screen.dart';
-export 'package:sonor/screens/library_screen.dart';
+export 'package:sonor/screens/home_screen.dart';
 export 'package:sonor/screens/playlist_screen.dart';
 export 'package:sonor/screens/songs_screen.dart';
