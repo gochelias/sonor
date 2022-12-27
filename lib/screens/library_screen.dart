23f7ace074e82f59d6e97e992a3e40f725882ecf
gochelias <56095957+gochelias@users.noreply.github.com>
2022-12-26 18:12:14 -0600

    feat: tabbar and views have been created for the library screen

--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LibraryScreen extends StatelessWidget {
@@ -5,14 +6,75 @@ class LibraryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.transparent,
-      body: SizedBox(
-        child: Center(
-          child: Text(
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: CupertinoColors.systemBackground.darkColor,
+          title: Text(
             'Library',
-            style: TextStyle(color: Colors.white),
+            style: TextStyle(
+              color: CupertinoColors.label.darkColor,
+              fontWeight: FontWeight.w600,
+            ),
           ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(50.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                isScrollable: true,
+                unselectedLabelColor: CupertinoColors.secondaryLabel.darkColor,
+                physics: const BouncingScrollPhysics(),
+                labelColor: CupertinoColors.white,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorColor: CupertinoColors.systemPink.darkColor,
+                splashFactory: NoSplash.splashFactory,
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                labelPadding: const EdgeInsets.only(
+                  left: 10.0,
+                  right: 10.0,
+                  bottom: 12.0,
+                ),
+                tabs: const <Widget>[
+                  Text('Playlists'),
+                  Text('Songs'),
+                  Text('Artists'),
+                  Text('Albums'),
+                ],
+              ),
+            ),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        body: const TabBarView(
+          physics: BouncingScrollPhysics(),
+          children: <Widget>[
+            Center(
+              child: Text(
+                'Playlists',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Songs',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Artists',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Albums',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
