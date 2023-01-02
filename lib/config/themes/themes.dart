e12d6faf29632fb7022063711f577402460594f3
gochelias <56095957+gochelias@users.noreply.github.com>
2023-01-02 12:59:08 -0600

    feat: color can now be changed dynamically depending on brightness

--- lib/config/themes/themes.dart
+++ lib/config/themes/themes.dart
@@ -1,2 +1,21 @@
+import 'package:flutter/material.dart';
+
 export 'package:sonor/config/themes/dark_theme.dart';
 export 'package:sonor/config/themes/light_theme.dart';
+
+class SonorTheme {
+  final BuildContext context;
+
+  SonorTheme(this.context);
+
+  Color dynamicColor({required Color color, required Color darkColor}) {
+    final Brightness brightness = Theme.of(context).brightness;
+
+    switch (brightness) {
+      case Brightness.light:
+        return color;
+      case Brightness.dark:
+        return darkColor;
+    }
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,9 +4,8 @@ import 'package:go_router/go_router.dart';
 
 import 'package:sonor/greetings.dart';
 import 'package:sonor/icons/icons.dart';
-import 'package:sonor/widgets/recently_played_witget.dart';
-import 'package:sonor/widgets/view_all_separator_widget.dart';
 import 'package:sonor/widgets/widgets.dart';
+import 'package:sonor/config/themes/themes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,65 +13,60 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80.0,
+        elevation: 0.0,
+        title: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              greetings(),
+              style: TextStyle(
+                color: SonorTheme(context).dynamicColor(
+                  color: CupertinoColors.label,
+                  darkColor: CupertinoColors.label.darkColor,
+                ),
+                fontSize: Theme.of(context).textTheme.titleMedium?.fontSize,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              'Let\'s play some music!',
+              style: TextStyle(
+                color: SonorTheme(context).dynamicColor(
+                  color: CupertinoColors.secondaryLabel,
+                  darkColor: CupertinoColors.secondaryLabel.darkColor,
+                ),
+                fontSize: 12.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          SonorIconButton(
+            onTap: () => context.goNamed('settings'),
+            icon: SonorIcons.settings_linear,
+            size: 24.0,
+            color: Colors.white,
+          ),
+          const SizedBox(width: 8.0),
+        ],
+      ),
       backgroundColor: Colors.transparent,
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         padding: const EdgeInsets.only(top: 6.0, bottom: 80.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              height: 100.0,
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        greetings(),
-                        style: TextStyle(
-                            color: CupertinoColors.label.darkColor,
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.w500),
-                      ),
-                      Text(
-                        'Let\'s play some music!',
-                        style: TextStyle(
-                          color: CupertinoColors.secondaryLabel.darkColor,
-                          fontSize: 14.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      const SonorIconButton(
-                        icon: SonorIcons.search_linear,
-                        size: 24.0,
-                        color: Colors.white,
-                      ),
-                      const SizedBox(width: 16.0),
-                      SonorIconButton(
-                        onTap: () => context.goNamed('settings'),
-                        icon: SonorIcons.settings_linear,
-                        size: 24.0,
-                        color: Colors.white,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            const ViewAllSeparator(
+          children: const <Widget>[
+            ViewAllSeparator(
               label: 'Recently played',
               routeName: 'playlist',
-              marginTop: 0.0,
+              marginTop: 16.0,
             ),
-            const RecentlyPlayed(),
+            RecentlyPlayed(),
           ],
         ),
       ),
