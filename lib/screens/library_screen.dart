394a4524278a8a8888f30f346fb52e41add985c7
gochelias <56095957+gochelias@users.noreply.github.com>
2023-01-03 13:17:11 -0600

    feat: changed the color of the labels in the tabbar and added actions to the appbar in the library screen

--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sonor/icons/icons.dart';
 
 import 'package:sonor/screens/screens.dart';
+import 'package:sonor/widgets/icon_button_widget.dart';
 
 class LibraryScreen extends StatelessWidget {
   const LibraryScreen({super.key});
@@ -12,31 +14,45 @@ class LibraryScreen extends StatelessWidget {
       length: 4,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: CupertinoColors.systemBackground.darkColor,
+          toolbarHeight: 70.0,
           title: Text(
-            'Library',
+            'Your Library',
             style: TextStyle(
               color: CupertinoColors.label.darkColor,
-              fontWeight: FontWeight.w600,
+              fontSize: 22.0,
+              fontWeight: FontWeight.normal,
             ),
           ),
+          actions: const <Widget>[
+            SonorIconButton(
+              icon: SonorIcons.search_linear,
+              color: CupertinoColors.white,
+            ),
+            SizedBox(width: 16.0),
+            SonorIconButton(
+              icon: SonorIcons.menu_bold,
+              color: CupertinoColors.white,
+            ),
+            SizedBox(width: 8.0),
+          ],
           bottom: PreferredSize(
             preferredSize: const Size.fromHeight(40.0),
             child: Align(
               alignment: Alignment.centerLeft,
               child: TabBar(
                 isScrollable: true,
-                unselectedLabelColor: CupertinoColors.secondaryLabel.darkColor,
-                physics: const BouncingScrollPhysics(),
-                labelColor: CupertinoColors.white,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicatorColor: CupertinoColors.systemPink.darkColor,
                 splashFactory: NoSplash.splashFactory,
+                physics: const BouncingScrollPhysics(),
+                indicatorSize: TabBarIndicatorSize.label,
+                labelColor: CupertinoColors.systemPink.darkColor,
+                labelStyle: const TextStyle(fontSize: 16.0),
+                unselectedLabelColor: CupertinoColors.secondaryLabel.darkColor,
+                indicatorColor: CupertinoColors.systemPink.darkColor,
                 padding: const EdgeInsets.symmetric(horizontal: 6.0),
                 labelPadding: const EdgeInsets.only(
                   left: 10.0,
                   right: 10.0,
-                  bottom: 12.0,
+                  bottom: 6.0,
                 ),
                 tabs: const <Widget>[
                   Text('Playlists'),
