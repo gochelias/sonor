1627b78e4f7f6bee6fd5f6cc97dd603c7a37292c
gochelias <56095957+gochelias@users.noreply.github.com>
2023-04-28 14:15:04 -0600

    feat: add playlist query

--- lib/widgets/add_to_playlist_widget.dart
+++ lib/widgets/add_to_playlist_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class AddToPlaylist extends StatelessWidget {
   const AddToPlaylist({
@@ -11,14 +12,12 @@ class AddToPlaylist extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final OnAudioQuery audioQuery = OnAudioQuery();
+
     return DraggableScrollableSheet(
-      expand: true,
       minChildSize: 0.5,
       initialChildSize: 1,
-      builder: (
-        BuildContext context,
-        ScrollController scrollController,
-      ) {
+      builder: (BuildContext context, ScrollController scrollController) {
         return Container(
           decoration: BoxDecoration(
             color: CupertinoColors.systemGrey6.darkColor,
@@ -48,36 +47,58 @@ class AddToPlaylist extends StatelessWidget {
                   ],
                 ),
               ),
-              Flexible(
-                child: ListView.separated(
-                  itemCount: 32,
-                  padding: const EdgeInsets.only(
-                    left: 24,
-                    right: 24,
-                    top: 16,
-                    bottom: 24,
-                  ),
-                  physics: const BouncingScrollPhysics(),
-                  separatorBuilder: (BuildContext context, int index) {
-                    return Divider(
-                      indent: 96,
-                      color: CupertinoColors.separator.darkColor,
-                    );
-                  },
-                  itemBuilder: (BuildContext context, int index) {
-                    return Row(
-                      children: <Widget>[
-                        Container(
-                          width: 80,
-                          height: 80,
-                          color: CupertinoColors.darkBackgroundGray,
-                        ),
-                        const SizedBox(width: 16),
-                        const Text('Playlist Name'),
-                      ],
-                    );
-                  },
+              FutureBuilder<List<PlaylistModel>>(
+                future: audioQuery.queryPlaylists(
+                  sortType: null,
+                  orderType: OrderType.ASC_OR_SMALLER,
+                  uriType: UriType.EXTERNAL,
+                  ignoreCase: true,
                 ),
+                builder: (
+                  BuildContext context,
+                  AsyncSnapshot<List<PlaylistModel>> snapshot,
+                ) {
+                  if (snapshot.data == null) {
+                    return const CircularProgressIndicator();
+                  }
+                  if (snapshot.data!.isEmpty) {
+                    return const Center(child: Text('Not Found'));
+                  }
+
+                  return Flexible(
+                    child: ListView.separated(
+                      controller: scrollController,
+                      itemCount: snapshot.data!.length,
+                      padding: const EdgeInsets.only(
+                        left: 24,
+                        right: 24,
+                        top: 16,
+                        bottom: 24,
+                      ),
+                      physics: const BouncingScrollPhysics(),
+                      separatorBuilder: (BuildContext context, int index) {
+                        return Divider(
+                          indent: 96,
+                          color: CupertinoColors.separator.darkColor,
+                        );
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        final PlaylistModel playlist = snapshot.data![index];
+                        return Row(
+                          children: <Widget>[
+                            Container(
+                              width: 80,
+                              height: 80,
+                              color: CupertinoColors.darkBackgroundGray,
+                            ),
+                            const SizedBox(width: 16),
+                            Text(playlist.playlist),
+                          ],
+                        );
+                      },
+                    ),
+                  );
+                },
               ),
             ],
           ),
